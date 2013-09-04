74e5b2348bd9ed05eba89df571f24f76136658ad
Nicolas <nicolasdascanio@gmail.com>
2013-09-04 11:58:21 -0300

    COMMIT PARA PRUEBA. Agregados tests dentro una clase y agregados 2 tests nuevos separados

---
+++ TestData/Java/TestUnitarioNumero2.java
@@ -0,0 +1,11 @@
+package ar.com.stomalab.metricastesis.tests;
+
+import org.junit.Test;
+
+public class TestUnitarioNumero2{
+	@Test
+	public void testSimple(){
+		int a = 1;
+        assertEquals(a, 1);
+	}
+}
