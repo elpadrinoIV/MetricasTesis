630abb361de9c70f4a6986594c4860224a9ce4cb
Nicolas <nicolasdascanio@gmail.com>
2013-09-04 11:55:15 -0300

    COMMIT PARA PRUEBA. Nueva prueba unitaria.

---
+++ TestData/Java/TestUnitarioNumero1.java
@@ -0,0 +1,11 @@
+package ar.com.stomalab.metricastesis.tests;
+
+import org.junit.Test;
+
+public class TestUnitarioNumero1{
+	@Test
+	public void testSimple(){
+		int a = 1;
+        assertEquals(a, 1);
+	}
+}
