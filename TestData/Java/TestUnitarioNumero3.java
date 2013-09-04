4c52b1cee185e419182fc5362b3a45ed7f8f20a4
Nicolas <nicolasdascanio@gmail.com>
2013-09-04 11:59:35 -0300

    COMMIT PARA PRUEBA. Agregados 4 tests (metodos) en 2 pruebas unitarias

--- TestData/Java/TestUnitarioNumero3.java
+++ TestData/Java/TestUnitarioNumero3.java
@@ -8,4 +8,28 @@ public class TestUnitarioNumero2{
 		int a = 1;
         assertEquals(a, 1);
 	}
+    
+    @Test
+	public void testSimple2(){
+		int a = 1;
+        assertEquals(a, 1);
+	}
+    
+    @Test
+	public void testSimple3(){
+		int a = 1;
+        assertEquals(a, 1);
+	}
+    
+    @Test
+	public void testSimple4(){
+		int a = 1;
+        assertEquals(a, 1);
+	}
+    
+    @Test
+	public void testSimple5(){
+		int a = 1;
+        assertEquals(a, 1);
+	}
 }
