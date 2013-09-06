071ae0d0d2baacb9f9ebbb4269f0b970271a7925
Nicolas Dascanio <nd@nd.com>
2013-09-05 23:58:37 -0300

    Corrijo AT y agrego fixture básico para que corra la AT

---
+++ lib/fixtures/contar_archivos_modificados_entre_commits.rb
@@ -0,0 +1,28 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require 'commits_handler'
+module Fixtures
+  class ContarArchivosModificadosEntreCommits
+    def initialize
+      path_repos = '/home/stoma/Documents/ruby/MetricasTesis'
+      @commits_handler = MetricasTesis::CommitsHandler.new(path_repos)
+    end
+
+    def set_hash_commit_desde hash
+      @commit_desde = hash
+    end
+
+    def set_hash_commit_hasta hash
+      @commit_hasta = hash
+    end
+
+    def set_filtro filtro
+      @filtro = filtro
+    end
+
+    def cantidad_archivos_modificados
+      0
+    end
+  end
+end
