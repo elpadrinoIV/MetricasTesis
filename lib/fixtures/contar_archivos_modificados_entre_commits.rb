8cba3d94dbbfd736da66f98597fb587bcf184931
Nicolas Dascanio <nd@nd.com>
2013-09-07 19:39:35 -0300

    Andando contar archs modificados entre commits, con AT pasando =)

--- lib/fixtures/contar_archivos_modificados_entre_commits.rb
+++ lib/fixtures/contar_archivos_modificados_entre_commits.rb
@@ -9,6 +9,7 @@ module Fixtures
     def initialize
       path_repos = '/home/stoma/Documents/ruby/MetricasTesis'
       @commits_handler = MetricasTesis::CommitsHandler.new(path_repos)
+      @archivos_commits_handler = MetricasTesis::ArchivosCommitsHandler.new(path_repos)
     end
 
     def set_hash_commit_desde hash
@@ -24,7 +25,9 @@ module Fixtures
     end
 
     def cantidad_archivos_modificados
-      0
+      lista_commits = @commits_handler.commits_entre_commits(@commit_desde, @commit_hasta, @filtro)
+      archivos_modificados = @archivos_commits_handler.get_archivos_cambiados_de_lista(lista_commits)
+      cantidad = archivos_modificados.values.inject(0) {|total,val| total + val.size}
     end
   end
 end
