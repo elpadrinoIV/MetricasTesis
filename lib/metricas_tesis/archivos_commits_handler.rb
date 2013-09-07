3c4e39993a35980b5068dd7f690f2747284db48a
Nicolas Dascanio <nd@nd.com>
2013-09-07 19:26:07 -0300

    Andando UT de archivos modificados entre commits

--- lib/metricas_tesis/archivos_commits_handler.rb
+++ lib/metricas_tesis/archivos_commits_handler.rb
@@ -3,10 +3,57 @@ module MetricasTesis
   class ArchivosCommitsHandler
     def initialize path_to_repo
       @path_to_repo = path_to_repo
+      @commits_handler = MetricasTesis::CommitsHandler.new path_to_repo
     end
 
-    def get_archivos_cambiados commit_hash
-      
+    ##
+    # Obtiene los archivos que cambiaron en un grupo de commits
+    # Params:
+    # +commit_desde+:: se toman los commits desde este commit inclusive
+    # +commit_hasta+:: se toman los commits hasta este commit inclusive
+    #
+    # Devuelve un hash con los archivos que fueron modificados en cada commit
+    #
+    # Lanza la excepción ArgumentError si:
+    # - No existe alguno de los commits
+    # - +commit_desde+ ocurre luego de +commit_hasta+
+    def get_archivos_cambiados (commit_desde, commit_hasta)
+      lista_commits = @commits_handler.commits_entre_commits(commit_desde, commit_hasta)
+
+      archivos_cambiados = Hash.new
+
+      lista_commits.each do |commit| 
+        archivos_cambiados_commit = `git diff --name-status #{commit}^ #{commit}`.split("\n")
+        archivos_cambiados_commit = self.filtrar_archivos(archivos_cambiados_commit, '^M')
+
+        archivos_cambiados[commit] = archivos_cambiados_commit
+      end
+
+      archivos_cambiados
+    end
+
+    ##
+    # Filtra que archivos cumplen con un cierto filtro.
+    # Params:
+    # +archivos+: lista de archivos con el formato +[AMD]+ path/al/archivo
+    # +filtro+: filtro a aplicar.
+    #
+    # Devuelve una lista con los archivos filtrados
+    #
+    # Ejemplo:
+    #   # Filtrar archivos modificados
+    #   filtrar_archivos (archs, '^M')
+    #
+    #   # Filtrar archivos dentro de la carpeta TestData
+    #   filtrar_archivos (archs, 'TestData/')
+    def filtrar_archivos (archivos, filtro)
+      archivos_filtrados = Array.new
+      archivos.each do |archivo|
+        if /#{filtro}/ =~ archivo
+          archivos_filtrados << archivo
+        end
+      end
+      archivos_filtrados
     end
   end
 end
--- test/archivos_cambiados_en_commit_test.rb
+++ test/archivos_cambiados_en_commit_test.rb
@@ -6,12 +6,63 @@ class ArchivosCambiadosEnCommitTest < Test::Unit::TestCase
     path_repos = '/home/stoma/Documents/ruby/MetricasTesis'
     @ach = MetricasTesis::ArchivosCommitsHandler.new(path_repos)
   end
-=begin
-  def test_ningun_archivo_cambiado_un_commit
-    commit_hash = '630abb361de9c70f4a6986594c4860224a9ce4cb'
-    archivos_cambiados = @ach.get_archivos_cambiados(commit_hash)
 
-    assert_equal(0, archivos_cambiados.size)
+  def test_ningun_archivo_cambiado_un_commit
+    commit_desde = '630abb361de9c70f4a6986594c4860224a9ce4cb'
+    commit_hasta = '630abb361de9c70f4a6986594c4860224a9ce4cb'
+
+    archivos_cambiados = @ach.get_archivos_cambiados(commit_desde, commit_hasta)
+
+    commits_esperados = 1
+    commits_obtenidos = archivos_cambiados.size
+    archivos_modificados_esperados = 0
+    archivos_modificados_obtenidos = archivos_cambiados.values.inject(0) {|total,val| total + val.size}
+
+    assert_equal(commits_esperados, commits_obtenidos)
+    assert_equal(archivos_modificados_esperados, archivos_modificados_obtenidos)
   end
-=end
+
+  def test_un_archivo_cambiado_un_commit
+    commit_desde = '74e5b2348bd9ed05eba89df571f24f76136658ad'
+    commit_hasta = '74e5b2348bd9ed05eba89df571f24f76136658ad'
+
+    archivos_cambiados = @ach.get_archivos_cambiados(commit_desde, commit_hasta)
+
+    commits_esperados = 1
+    commits_obtenidos = archivos_cambiados.size
+    archivos_modificados_esperados = 1
+    archivos_modificados_obtenidos = archivos_cambiados.values.inject(0) {|total,val| total + val.size}
+
+    assert_equal(commits_esperados, commits_obtenidos)
+    assert_equal(archivos_modificados_esperados, archivos_modificados_obtenidos)
+  end
+
+  def varios_archivos_cambiados_un_commit
+    commit_desde = '4c52b1cee185e419182fc5362b3a45ed7f8f20a4'
+    commit_hasta = '4c52b1cee185e419182fc5362b3a45ed7f8f20a4'
+
+    archivos_cambiados = @ach.get_archivos_cambiados(commit_desde, commit_hasta)
+
+    commits_esperados = 1
+    commits_obtenidos = archivos_cambiados.size
+    archivos_modificados_esperados = 2
+    archivos_modificados_obtenidos = archivos_cambiados.values.inject(0) {|total,val| total + val.size}
+
+    assert_equal(commits_esperados, commits_obtenidos)
+    assert_equal(archivos_modificados_esperados, archivos_modificados_obtenidos)
+  end
+
+  def varios_archivos_cambiados_varios_commits
+    commit_desde = '630abb361de9c70f4a6986594c4860224a9ce4cb'
+    commit_desde = '3de42f9222f93495088347120049ce550ba0a586'
+    archivos_cambiados = @ach.get_archivos_cambiados(commit_desde, commit_hasta)
+
+    commits_esperados = 4
+    commits_obtenidos = archivos_cambiados.size
+    archivos_modificados_esperados = 4
+    archivos_modificados_obtenidos = archivos_cambiados.values.inject(0) {|total,val| total + val.size}
+
+    assert_equal(commits_esperados, commits_obtenidos)
+    assert_equal(archivos_modificados_esperados, archivos_modificados_obtenidos)
+  end
 end
