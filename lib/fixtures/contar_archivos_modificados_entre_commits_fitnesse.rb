260a789af4a3fa393ee765dc16c743b0ad954e85
Nicolas Dascanio <nd@nd.com>
2013-09-07 23:55:43 -0300

    AT pero usando fitnesse, hay un problema, hay que probar un approach diferente

---
+++ lib/fixtures/contar_archivos_modificados_entre_commits_fitnesse.rb
@@ -0,0 +1,29 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require 'archivos_commits_handler'
+require 'commits_handler'
+
+module Fixtures
+  class ContarArchivosModificadosEntreCommitsFitnesse
+    def initialize
+      path_repos = '../fitnesse/.git'
+      @commits_handler = MetricasTesis::CommitsHandler.new(path_repos)
+      @archivos_commits_handler = MetricasTesis::ArchivosCommitsHandler.new(path_repos)
+    end
+
+    def set_hash_commit_desde hash
+      @commit_desde = hash
+    end
+
+    def set_hash_commit_hasta hash
+      @commit_hasta = hash
+    end
+
+    def cantidad_archivos_modificados
+      lista_commits = @commits_handler.commits_entre_commits(@commit_desde, @commit_hasta)
+      archivos_modificados = @archivos_commits_handler.get_archivos_cambiados_de_lista(lista_commits)
+      cantidad = archivos_modificados.values.inject(0) {|total,val| total + val.size}
+    end
+  end
+end
