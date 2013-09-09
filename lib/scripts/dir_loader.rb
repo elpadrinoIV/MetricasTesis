fe2d64eead58b7f9eb910d4a1f1810894c0ddc91
Nicolas Dascanio <nd@nd.com>
2013-09-08 23:44:06 -0300

    loader de directorios

--- lib/scripts/dir_loader.rb
+++ lib/scripts/dir_loader.rb
@@ -1,14 +1,27 @@
 module Scripts
   class DirLoader
     def initialize
+      @directorios = Hash.new
     end
 
     def cargar_datos_de_archivo path_archivo
-      
+      lineas = IO.readlines(path_archivo)
+      lineas.each do |linea|
+        if /^\s*(?<nombre>[a-zA-Z0-9_-]+)\s*=\s*(?<dir>.+)$/ =~ linea
+          if @directorios.has_key? nombre
+            raise IndexError, 'nombre clave repetido'
+          end
+
+          @directorios[nombre] = dir
+        end
+      end
     end
 
     def get_directorio nombre
-      
+      if !@directorios.has_key? nombre
+        raise ArgumentError, "no hay directorio para #{nombre}"
+      end
+      @directorios[nombre]
     end
   end
 end
--- test/scripts/dir_loader_test.rb
+++ test/scripts/dir_loader_test.rb
@@ -7,21 +7,23 @@ class DirLoaderTest < Test::Unit::TestCase
   end
 
   def test_cargar_datos_desde_archivo
-    @dir_loader.cargar_datos_de_archivo('../../scripts/directorios')
+    path = File.dirname(__FILE__) + '/../../lib/scripts/directorios'
+    @dir_loader.cargar_datos_de_archivo(path)
 
     assert_equal('../../Resultados', @dir_loader.get_directorio('RESULTADOS'))
     assert_equal('../../Resultados/Data', @dir_loader.get_directorio('DATA'))
-    assert_equal('../../Resultados/IMAGENES', @dir_loader.get_directorio('IMAGENES'))
+    assert_equal('../../Resultados/Imagenes', @dir_loader.get_directorio('IMAGENES'))
   end
 
   def test_cargar_datos_archivo_inexistente
-    assert_raise(IOError) do
+    assert_raise(Errno::ENOENT) do
       @dir_loader.cargar_datos_de_archivo('archivo_inexistente')
     end
   end
 
   def test_cargar_pedir_directorio_que_no_existe
-    @dir_loader.cargar_datos_de_archivo('../../scripts/directorios')
+    path = File.dirname(__FILE__) + '/../../lib/scripts/directorios'
+    @dir_loader.cargar_datos_de_archivo(path)
 
     assert_raise(ArgumentError) do
       @dir_loader.get_directorio('DIRECTORIO_INEXISTENTE')
