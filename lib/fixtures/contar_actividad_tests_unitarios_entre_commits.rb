54fe58be0bf94eecc1b86f99761dd5de53fdc62b
Nicolas Dascanio <nd@nd.com>
2013-09-08 23:58:51 -0300

    AT para contar pruebas unitarias (en vez de archivos sin discriminar)

---
+++ lib/fixtures/contar_actividad_tests_unitarios_entre_commits.rb
@@ -0,0 +1,52 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require 'archivos_commits_handler'
+require 'commits_handler'
+
+module Fixtures
+  class ContarActividadTestsUnitariosEntreCommits
+    def initialize
+      path_repos = './.git'
+      @commits_handler = MetricasTesis::CommitsHandler.new(path_repos)
+      @archivos_commits_handler = MetricasTesis::ArchivosCommitsHandler.new(path_repos)
+    end
+
+    def set_hash_commit_desde hash
+      @commit_desde = hash
+    end
+
+    def set_hash_commit_hasta hash
+      @commit_hasta = hash
+    end
+
+    def set_filtro filtro
+      @filtro = filtro
+    end
+
+    def set_patron patron
+      @patron = patron
+    end
+
+    def cantidad_archivos_agregados
+      # lista_commits = @commits_handler.commits_entre_commits(@commit_desde, @commit_hasta, @filtro)
+      # archivos_modificados = @archivos_commits_handler.get_archivos_de_lista(lista_commits, ['M'])
+      # cantidad = archivos_modificados.values.inject(0) {|total,val| total + val.size}
+      0
+    end
+
+    def cantidad_archivos_modificados
+      # lista_commits = @commits_handler.commits_entre_commits(@commit_desde, @commit_hasta, @filtro)
+      # archivos_modificados = @archivos_commits_handler.get_archivos_de_lista(lista_commits, ['M'])
+      # cantidad = archivos_modificados.values.inject(0) {|total,val| total + val.size}
+      0
+    end
+
+    def cantidad_archivos_eliminados
+      # lista_commits = @commits_handler.commits_entre_commits(@commit_desde, @commit_hasta, @filtro)
+      # archivos_modificados = @archivos_commits_handler.get_archivos_de_lista(lista_commits, ['M'])
+      # cantidad = archivos_modificados.values.inject(0) {|total,val| total + val.size}
+      0
+    end
+  end
+end
