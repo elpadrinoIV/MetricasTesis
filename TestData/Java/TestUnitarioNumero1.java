235f30edecee9fc41bc8817b34c3fe56900e5108
Nicolas Dascanio <nd@nd.com>
2013-09-22 00:16:17 -0300

    COMMIT PARA PRUEBA. Cambiados tabs por espacios

--- TestData/Java/TestUnitarioNumero1.java
+++ TestData/Java/TestUnitarioNumero1.java
@@ -3,34 +3,34 @@ package ar.com.stomalab.metricastesis.tests;
 import org.junit.Test;
 
 public class TestUnitarioNumero1{
-	@Test
-	public void testSimple(){
-		int a = 1;
+    @Test
+    public void testSimple(){
+        int a = 1;
         assertEquals(a, 1);
-	}
+    }
     
     @Test
-	public void testSimple2(){
-		int a = 2;
+    public void testSimple2(){
+        int a = 2;
         assertEquals(a, 2);
-	}
+    }
     
     @Test
-	public void testSimple3(){
-		int b = 3;
+    public void testSimple3(){
+        int b = 3;
         assertEquals(b, 3);
-	}
+    }
     
     @Test
-	public void testSimple4(){
-		int a = 1;
+    public void testSimple4(){
+        int a = 1;
         int b = 2 - 1;
         assertEquals(a, b);
-	}
+    }
     
     @Test
-	public void testSimple5(){
-		int a = 1;
+    public void testSimple5(){
+        int a = 1;
         assertEquals(a, 1);
-	}
+    }
 }
