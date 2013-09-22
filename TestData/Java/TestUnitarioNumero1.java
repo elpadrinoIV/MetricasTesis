d3f4b08cd8e9970429632ea619330b970175f7a4
Nicolas Dascanio <nd@nd.com>
2013-09-22 00:38:22 -0300

    COMMIT PARA PRUEBA. Eliminando comentarios largos

--- TestData/Java/TestUnitarioNumero1.java
+++ TestData/Java/TestUnitarioNumero1.java
@@ -2,24 +2,15 @@ package ar.com.stomalab.metricastesis.tests;
 
 import org.junit.Test;
 
-/* Modificando comentario largo de una linea */
 public class TestUnitarioNumero1{
     @Test
     public void testSimple(){
         int a = 1;
-        /*
-         * Comentario largo de
-         * varias lineas
-         */
         assertEquals(a, 1);
     }
 
     @Test
     public void testSimple2(){
-        /*
-         Modificando comentario largo de varias liena
-
-         */
         int a = 2;
         assertEquals(a, 2);
     }
@@ -32,21 +23,15 @@ public class TestUnitarioNumero1{
 
     @Test
     public void testSimple4(){
-        /* Modificando comentario largo
-         que tiene // dos barras
-         y una apertura nueva /* que
-         no hace nada
-                        */
         int a = 1;
         int b = 2 - 1;
-        assertEquals(a, b); /* Modificando Comentario largo al final de una linea */  
+        assertEquals(a, b);
     }
 
     @Test
     public void testSimple5(){
         int a = 1;
-        assertEquals(a, 1); /* Modificando Comentario largo al final de una linea 
-                               que sigue por varias lineas */
+        assertEquals(a, 1); 
     }
 
 }
