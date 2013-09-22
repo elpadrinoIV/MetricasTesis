20240ce95a722f8174aa503c09b6053f27139032
Nicolas Dascanio <nd@nd.com>
2013-09-22 01:00:28 -0300

    COMMIT PARA PRUEBA. Comento con comentario largo varios tests y lineas y una qu eno tiene nada

--- TestData/Java/TestUnitarioNumero1.java
+++ TestData/Java/TestUnitarioNumero1.java
@@ -9,32 +9,35 @@ public class TestUnitarioNumero1{
         String cadena = "modifico prueba cadena";
         assertEquals(a, 1);
     }
-
+/* */
     @Test
     public void testSimple2(){
         int c = 4;
-        String cadena = "prueba cadena";
-        assertEquals(c, 2);
+        /*String cadena = "prueba cadena";*/
+        /* assertEquals(c, 2); */
     }
 
-    @Test
+    /* @Test
     public void testSimple3(){
         int b = 3;
         String cadena4 = "modifico prueba cadena";
         String cadena5 = "modifico prueba cadena";
         assertEquals(b, 3);
-    }
+    } */
 
+    /*
     @Test
     public void testSimple4(){
         int a = 1;
         int b = 2 - 1;
         assertEquals(a, b);
     }
+    */
 
+    /*
     @Test
     public void testSimple6(){
         int a = 1;
         assertEquals(a, 1);
-    }
+    }*/
 }
