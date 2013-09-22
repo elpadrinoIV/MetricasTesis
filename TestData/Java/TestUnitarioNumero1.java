4101d499febf8a4dbf1959dacca37d03751c521f
Nicolas Dascanio <nd@nd.com>
2013-09-22 00:23:41 -0300

    COMMIT PARA PRUEBA. Agregando espacios y quitando espacios

--- TestData/Java/TestUnitarioNumero1.java
+++ TestData/Java/TestUnitarioNumero1.java
@@ -2,30 +2,30 @@ package ar.com.stomalab.metricastesis.tests;
 
 import org.junit.Test;
 
-public class TestUnitarioNumero1{
+public class TestUnitarioNumero1 {
     @Test
-    public void testSimple(){
-        int a = 1;
-        assertEquals(a, 1);
+    public void testSimple() {
+        int a=1;
+        assertEquals(a,1);
     }
 
     @Test
-    public void testSimple2(){
-        int a = 2;
-        assertEquals(a, 2);
+    public void testSimple2() {
+        int a=2;
+        assertEquals(a,2);
     }
     
-    @Test
-    public void testSimple3(){
-        int b = 3;
-        assertEquals(b, 3);
+        @Test
+    public void testSimple3() {
+        int          b=3;
+        assertEquals  (b,3);
     }
 
     @Test
     public void testSimple4(){
         int a = 1;
         int b = 2 - 1;
-        assertEquals(a, b);
+           assertEquals    (   a    ,     b     )    ;   
     }
 
     @Test
