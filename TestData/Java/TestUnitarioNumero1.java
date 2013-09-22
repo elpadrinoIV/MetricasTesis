492df8bfd08bc2ff7ee316febf0c0995276fb8f5
Nicolas Dascanio <nd@nd.com>
2013-09-22 00:40:53 -0300

    COMMIT PARA PRUEBA. Agregando lineas.
    - Agrego una linea en testSimple
    - Agrego dos lineas en testSimple2
    - Agrego tres lineas en testSimple3

--- TestData/Java/TestUnitarioNumero1.java
+++ TestData/Java/TestUnitarioNumero1.java
@@ -6,18 +6,24 @@ public class TestUnitarioNumero1{
     @Test
     public void testSimple(){
         int a = 1;
+        String cadena = "prueba cadena";
         assertEquals(a, 1);
     }
 
     @Test
     public void testSimple2(){
         int a = 2;
+        int b = 3;
+        String cadena = "prueba cadena";
         assertEquals(a, 2);
     }
 
     @Test
     public void testSimple3(){
         int b = 3;
+        String cadena1 = "prueba cadena";
+        String cadena2 = "prueba cadena";
+        String cadena3 = "prueba cadena";
         assertEquals(b, 3);
     }
 
