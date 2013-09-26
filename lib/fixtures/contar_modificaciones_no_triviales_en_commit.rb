aa3d7fa356767566efa9fb1cc5d1bc73939f1e47
Nicolas Dascanio <nd@nd.com>
2013-09-25 23:40:26 -0300

    AT ahora pregunta si se modifico algo, no que lineas

--- lib/fixtures/contar_modificaciones_no_triviales_en_commit.rb
+++ lib/fixtures/contar_modificaciones_no_triviales_en_commit.rb
@@ -8,16 +8,8 @@ module Fixtures
       @commit_hash = commit_hash
     end
 
-    def lineas_modificadas
-      0
-    end
-
-    def lineas_agregadas
-      0
-    end
-
-    def lineas_eliminadas
-      0
+    def archivos_modificados
+      false
     end
   end
 end
