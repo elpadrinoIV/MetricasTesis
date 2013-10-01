bec0149651a2ed80640f0616b5a7cd8bc4fdfb78
Nicolas Dascanio <nd@nd.com>
2013-10-01 18:47:54 -0300

    refactor extraccion de metodo

--- lib/metricas_tesis/analizador_modificaciones_java.rb
+++ lib/metricas_tesis/analizador_modificaciones_java.rb
@@ -14,33 +14,37 @@ module MetricasTesis
       archivos_modificados[commit_hash].each do |archivo|
         archivo.gsub!(/^M\s*/,'')
         
-        archivo_version_anterior = `git show #{commit_hash}^:#{archivo}`.split("\n")
-        archivo_version_nueva = `git show #{commit_hash}:#{archivo}`.split("\n")
-
-        archivo_version_anterior = @normalizador_archivos_java.normalizar(archivo_version_anterior)
-        archivo_version_nueva = @normalizador_archivos_java.normalizar(archivo_version_nueva)
-
-        # Guardo los archivos temporalmente
-        File.open('__TEMP__VERSION_ANTERIOR', 'w') do |file|
-          archivo_version_anterior.each {|linea| file.write(linea + "\n")}
-        end
-
-        File.open('__TEMP__VERSION_NUEVA', 'w') do |file|
-          archivo_version_nueva.each {|linea| file.write(linea + "\n")}
-        end
-
-        `diff  --ignore-all-space --ignore-blank-lines __TEMP__VERSION_ANTERIOR __TEMP__VERSION_NUEVA`; hay_diferencias=$?.success?
-        File.delete('__TEMP__VERSION_ANTERIOR')
-        File.delete('__TEMP__VERSION_NUEVA')
-
-        # diff arroja 0 cuando no hay diferencias y 1 cuando hay diferencias,
-        # por eso lo niego.
-        hay_diferencias = !hay_diferencias
-        
-        cantidad_archivos_modificados += 1 if hay_diferencias
+        cantidad_archivos_modificados += 1 if hay_cambio_no_trivial?(archivo, commit_hash)
       end
       
       cantidad_archivos_modificados
+    end
+
+    def hay_cambio_no_trivial? archivo, commit_hash
+      archivo_version_anterior = `git show #{commit_hash}^:#{archivo}`.split("\n")
+      archivo_version_nueva = `git show #{commit_hash}:#{archivo}`.split("\n")
+
+      archivo_version_anterior = @normalizador_archivos_java.normalizar(archivo_version_anterior)
+      archivo_version_nueva = @normalizador_archivos_java.normalizar(archivo_version_nueva)
+
+      # Guardo los archivos temporalmente
+      File.open('__TEMP__VERSION_ANTERIOR', 'w') do |file|
+        archivo_version_anterior.each {|linea| file.write(linea + "\n")}
+      end
+
+      File.open('__TEMP__VERSION_NUEVA', 'w') do |file|
+        archivo_version_nueva.each {|linea| file.write(linea + "\n")}
+      end
+
+      `diff  --ignore-all-space --ignore-blank-lines __TEMP__VERSION_ANTERIOR __TEMP__VERSION_NUEVA`; hay_diferencias=$?.success?
+      File.delete('__TEMP__VERSION_ANTERIOR')
+      File.delete('__TEMP__VERSION_NUEVA')
+
+      # diff arroja 0 cuando no hay diferencias y 1 cuando hay diferencias,
+      # por eso lo niego.
+      hay_diferencias = !hay_diferencias
+
+      hay_diferencias
     end
   end
 
