9780ff65b3a02dd6eaeb72076d37b7bf6be6adc7
Nicolás Dascanio <nd@nd.com>
2013-10-25 16:38:38 -0300

    script para convertir tablas a latex

---
+++ lib/scripts/utilitarios/table_to_latex.rb
@@ -0,0 +1,72 @@
+require 'table_to_array'
+require 'array_to_table'
+module MetricasTesis
+  module Scripts
+    module Utilitarios
+      class TableToLatex
+        def initialize          
+        end
+
+        def self.convertir tabla
+          tabla_latex = Array.new
+          tabla_para_csv = MetricasTesis::Scripts::Utilitarios::ArrayToTable.convertir(tabla)
+
+          # Encabezado
+          tabla_latex << '\setlength{\tabcolsep}{10pt}'
+          tabla_latex << '\rowcolors{2}{grey}{white}'
+          tabla_latex << '\begin{table}[!htpb]'
+          tabla_latex << '\centering'
+
+          cantidad_columnas = tabla.first.keys.size
+          tabla_latex << '\begin{tabular}{' + 'c'*cantidad_columnas + '}'
+          tabla_latex << '\toprule'
+
+          # Contenido tabla
+          # Nombres columnas
+          fila_nombres_columnas = tabla_para_csv.shift
+          fila_latex = ""
+          fila_nombres_columnas.each do |columna|
+            fila_latex += "\\multicolumn{1}{c}{\\textbf{#{columna}}} & "
+          end
+          # puse un & de más, lo saco y pongo el fin de linea
+          fila_latex.gsub!(/\s*&\s*$/, '\\\\\\\\')
+          tabla_latex << fila_latex
+          
+          tabla_latex << '\midrule'
+
+          # el resto del contenido de la tabla
+          tabla_para_csv.each do |fila|
+            fila_latex = ""
+            fila.each do |columna|
+              fila_latex += "#{columna} & "
+            end
+            # puse un & de más, lo saco y pongo el fin de linea
+            fila_latex.gsub!(/\s*&\s*$/, '\\\\\\\\')
+            tabla_latex << fila_latex
+          end
+    
+          # Fin tabla
+          tabla_latex << '\bottomrule'
+          tabla_latex << '\end{tabular}'
+          tabla_latex << '\caption{}'
+          tabla_latex << '\label{tbl:}'
+          tabla_latex << '\end{table}'
+        end
+
+      end
+    end
+  end
+end
+
+
+if "RUN_SCRIPT" == ARGV[0] && /\.csv$/ =~ ARGV[1]
+  path_archivo = ARGV[1]
+  archivo = File.readlines(path_archivo)
+  tabla = MetricasTesis::Scripts::Utilitarios::TableToArray.convertir(archivo, "\t")
+  tabla_latex = MetricasTesis::Scripts::Utilitarios::TableToLatex.convertir(tabla)
+  
+  path_archivo_latex = path_archivo.gsub(/\.csv$/, '.tex')
+  archivo_latex = File.open(path_archivo_latex, 'w')
+  tabla_latex.each { |linea| archivo_latex.puts linea }
+  archivo_latex.close  
+end
---
+++ test/scripts/table_to_latex_test.rb
@@ -0,0 +1,41 @@
+require 'test/unit'
+require 'array_to_table'
+require 'table_to_array'
+require 'table_to_latex'
+
+class TableToLatexTest < Test::Unit::TestCase
+  def setup
+  end
+  
+  def test_conversion_desde_tabla
+    tabla = Array.new
+    tabla << {:col1 => "AAA", :col2 => "BBB", :col3 => "CCC"}
+    tabla << {:col1 => "DDD", :col2 => "EEE", :col3 => "FFF"}
+
+    resultado_obtenido = MetricasTesis::Scripts::Utilitarios::TableToLatex.convertir(tabla)
+    resultado_esperado = get_resultado_esperado()
+
+    assert_equal(resultado_esperado, resultado_obtenido)
+  end
+
+  def get_resultado_esperado
+    resultado = Array.new
+    resultado << '\setlength{\tabcolsep}{10pt}'
+    resultado << '\rowcolors{2}{grey}{white}'
+    resultado << '\begin{table}[!htpb]'
+    resultado << '\centering'
+    resultado << '\begin{tabular}{ccc}'
+    resultado << '\toprule'
+    resultado << '\multicolumn{1}{c}{\textbf{col1}} & \multicolumn{1}{c}{\textbf{col2}} & \multicolumn{1}{c}{\textbf{col3}}\\\\'
+    resultado << '\midrule'
+    resultado << 'AAA & BBB & CCC\\\\'
+    resultado << 'DDD & EEE & FFF\\\\'
+    resultado << '\bottomrule'
+    resultado << '\end{tabular}'
+    resultado << '\caption{}'
+    resultado << '\label{tbl:}'
+    resultado << '\end{table}'
+
+    resultado
+  end
+end
