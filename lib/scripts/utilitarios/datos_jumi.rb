ca98303dbe8f84272461bfba45454f01fa2b5f03
Nicolas Dascanio <nd@nd.com>
2013-10-27 20:45:47 -0300

    filtrados algunos tags de jumi que no eran releases, corro los scripts de nuevo

--- lib/scripts/utilitarios/datos_jumi.rb
+++ lib/scripts/utilitarios/datos_jumi.rb
@@ -13,7 +13,12 @@ module MetricasTesis
           @pattern_acceptance_tests = MetricasTesis::Scripts::Utilitarios::JumiFilePatterns.get_patron_pruebas_aceptacion
           @pattern_unit_tests = MetricasTesis::Scripts::Utilitarios::JumiFilePatterns.get_patron_pruebas_unitarias
           @pattern_codigo = MetricasTesis::Scripts::Utilitarios::JumiFilePatterns.get_patron_codigo
-          @lista_excluded_tags = []
+          @lista_excluded_tags = ['v0.1.46',
+            'v0.1.64',
+            'v0.2.241',
+            'v0.4.317',
+            'v0.4.350',
+            'v0.5.376'] # los primeros 2 no son releases, los otros son tags con pocos commits dps de otro
           @lista_excluded_commits = []
           @lista_excluded_files = ['jumi-core/src/test/java/fi/jumi/core/files/FileNamePatternTestClassFinderTest.java',
             'jumi-core/src/test/java/fi/jumi/core/discovery/PathMatcherTestFileFinderTest.java'
